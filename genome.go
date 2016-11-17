ce18c26b6721197d355768315ff367f9219b8d82
jinyeom <jinyeom95@gmail.com>
2016-11-17 14:36:56 -0600

    progress

--- genome.go
+++ genome.go
@@ -21,12 +21,13 @@ func NewGenome(numSensor, numOutput int) *Genome {
 	numNodes := numSensor + 1 + numOutput
 	numConns := (numSensor + 1) * numOutput
 
-	// initialize sensor nodes and output nodes
 	nodes := make([]*NodeGene, 0, numNodes)
 	conns := make([]*ConnGene, 0, numConns)
+	// sensor nodes
 	for i := 0; i < numSensor; i++ {
 		nodes = append(nodes, NewNode(i, "sensor"))
 	}
+	// output nodes and connections
 	nodes = append(nodes, NewNode(numNodes-1, "bias"))
 	for i := numSensor + 1; i < numNodes; i++ {
 		nodes = append(nodes, NewNode(i, "output"))
@@ -47,19 +48,41 @@ func NewGenome(numSensor, numOutput int) *Genome {
 	}
 }
 
+// NodeGene is an implementation of each node within a genome.
+// Each node includes a node ID (NID), a node type (NType), and
+// a pointer to an activation function.
 type NodeGene struct {
-	nid int // node ID
+	nid   int             // node ID
+	ntype string          // node type
+	afn   *ActivationFunc // activation function
 }
 
-// NewNodeGene creates a new node gene with the given NID.
-func NewNodeGene(nid int, ntype string) *NodeGene {
+// NewNodeGene creates a new node gene with the given NID, node type, and
+// a pointer to an activation function.
+func NewNodeGene(nid int, ntype string, afn *ActivationFunc) *NodeGene {
 	return &NodeGene{
 		nid:   nid,
 		ntype: ntype,
+		afn:   afn,
 	}
 }
 
+// ConnGene is an implementation of each connection within a genome.
+// It represents a connection between an in-node and an out-node;
+// it contains an innovation number and nids of the in-node and the
+// out-node.
 type ConnGene struct {
-	innov   int // innovation number
-	in, out int // in node and out node
+	innov int // innovation number
+	in    int // NID of in-node
+	out   int // NID of out-node
+}
+
+// NewConnGene creates a new connection gene with the given innovation
+// number, the in-node NID, and the out-node NID.
+func NewConnGene(innov, in, out int) *ConnGene {
+	return &ConnGene{
+		innov: innov,
+		in:    in,
+		out:   out,
+	}
 }
