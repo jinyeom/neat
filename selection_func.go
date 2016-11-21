20537b42975b618c2368fa3d61e333588f00b548
jinyeom <jinyeom95@gmail.com>
2016-11-21 03:40:12 -0600

    progress

--- selection_func.go
+++ selection_func.go
@@ -1,3 +1,38 @@
+/*
+
+
+selection_func.go implementation of selection function type.
+
+@licstart   The following is the entire license notice for
+the Go code in this page.
+
+Copyright (C) 2016 jin yeom, whitewolf.studio
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+As additional permission under GNU GPL version 3 section 7, you
+may distribute non-source (e.g., minimized or compacted) forms of
+that code without the copy of the GNU GPL normally required by
+section 4, provided you include this license notice and a URL
+through which recipients can access the Corresponding Source.
+
+@licend    The above is the entire license notice
+for the Go code in this page.
+
+
+*/
+
 package neat
 
 // SelectionFunc is a type of function that selects a genome
