e9edc3c7269aa886089e2957acd245ec9b6a1db6
jinyeom <jinyeom95@gmail.com>
2017-05-18 09:07:04 -0500

    progress

--- evaluation_func.go
+++ evaluation_func.go
@@ -1,5 +1,58 @@
 package neat
 
+import (
+	"log"
+	"math"
+)
+
 // EvaluationFunc is a type of function that evaluates an argument neural
 // network and returns a its fitness (performance) score.
 type EvaluationFunc func(*NeuralNetwork) float64
+
+// XORTest returns an XOR test as an evaluation function.
+func XORTest() EvaluationFunc {
+	return func(n *NeuralNetwork) float64 {
+		score := 0.0
+
+		inputs := make([]float64, 3)
+		inputs[0] = 1.0 // bias
+
+		// 0 xor 0
+		inputs[1] = 0.0
+		inputs[2] = 0.0
+		output, err := n.FeedForward(inputs)
+		if err != nil {
+			log.Fatal(err)
+		}
+		score += math.Pow((output[0] - 0.0), 2.0)
+
+		// 0 xor 1
+		inputs[1] = 0.0
+		inputs[2] = 1.0
+		output, err = n.FeedForward(inputs)
+		if err != nil {
+			log.Fatal(err)
+		}
+		score += math.Pow((output[0] - 1.0), 2.0)
+
+		// 1 xor 0
+		inputs[1] = 1.0
+		inputs[2] = 0.0
+		output, err = n.FeedForward(inputs)
+		if err != nil {
+			log.Fatal(err)
+		}
+		score += math.Pow((output[0] - 1.0), 2.0)
+
+		// 1 xor 1
+		inputs[1] = 1.0
+		inputs[2] = 1.0
+		output, err = n.FeedForward(inputs)
+		if err != nil {
+			log.Fatal(err)
+		}
+		score += math.Pow((output[0] - 0.0), 2.0)
+
+		return score
+	}
+}
--- neat_test.go
+++ neat_test.go
@@ -2,8 +2,6 @@ package neat
 
 import (
 	"fmt"
-	"log"
-	"math"
 	"math/rand"
 	"testing"
 )
@@ -19,50 +17,12 @@ func NEATUnitTest() {
 	config.Summarize()
 
 	fmt.Println("\x1b[32m=Testing creating and running NEAT...\x1b[0m")
-	New(config, func(n *NeuralNetwork) float64 {
-		score := 0.0
+	n := New(config, XORTest())
+	n.Run(true)
 
-		inputs := make([]float64, 3)
-		inputs[0] = 1.0 // bias
-
-		// 0 xor 0
-		inputs[1] = 0.0
-		inputs[2] = 0.0
-		output, err := n.FeedForward(inputs)
-		if err != nil {
-			log.Fatal(err)
-		}
-		score += math.Pow((output[0] - 0.0), 2.0)
-
-		// 0 xor 1
-		inputs[1] = 0.0
-		inputs[2] = 1.0
-		output, err = n.FeedForward(inputs)
-		if err != nil {
-			log.Fatal(err)
-		}
-		score += math.Pow((output[0] - 1.0), 2.0)
-
-		// 1 xor 0
-		inputs[1] = 1.0
-		inputs[2] = 0.0
-		output, err = n.FeedForward(inputs)
-		if err != nil {
-			log.Fatal(err)
-		}
-		score += math.Pow((output[0] - 1.0), 2.0)
-
-		// 1 xor 1
-		inputs[1] = 1.0
-		inputs[2] = 1.0
-		output, err = n.FeedForward(inputs)
-		if err != nil {
-			log.Fatal(err)
-		}
-		score += math.Pow((output[0] - 0.0), 2.0)
-
-		return score
-	}).Run(true)
+	for _, g := range n.HallOfFame.BestGenomes {
+		fmt.Println(g.String())
+	}
 
 }
 
