58781d0ca854744a4034ab789f85fd04e66d56c8
jinyeom <jinyeom95@gmail.com>
2017-05-23 16:42:12 -0500

    added copyright statements

--- activation_func.go
+++ activation_func.go
@@ -1,37 +1,19 @@
-/*
-
-
-activation_func.go implementation of activation functions used in a network.
-
-@licstart   The following is the entire license notice for
-the Go code in this page.
-
-Copyright (C) 2017 Jin Yeom
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-As additional permission under GNU GPL version 3 section 7, you
-may distribute non-source (e.g., minimized or compacted) forms of
-that code without the copy of the GNU GPL normally required by
-section 4, provided you include this license notice and a URL
-through which recipients can access the Corresponding Source.
-
-@licend    The above is the entire license notice
-for the Go code in this page.
-
-
-*/
+// activation_func.go implementation of activation functions used in a network.
+//
+// Copyright (C) 2017  Jin Yeom
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package neat
 
